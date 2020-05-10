a31bab38a0a805580db98e54734aee31dc584748
JoeMcRainbow <35050901+JoeMcRainbow@users.noreply.github.com>
2020-05-10 14:49:12 -0500

    Updated website to match local copy

--- js/scripts.js
+++ js/scripts.js
@@ -9,16 +9,7 @@
     "use strict"; 
 	
 	/* Preloader */
-	$(window).on('load', function() {
-		var preloaderFadeOutTime = 500;
-		function hidePreloader() {
-			var preloader = $('.spinner-wrapper');
-			setTimeout(function() {
-				preloader.fadeOut(preloaderFadeOutTime);
-			}, 500);
-		}
-		hidePreloader();
-	});
+
 
 	
 	/* Navbar Scripts */
